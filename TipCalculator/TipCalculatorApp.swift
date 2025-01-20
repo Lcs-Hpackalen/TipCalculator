ef51d62c34135ab47826e30d7fcb647fbc9fd532
Hannu Packalen <hpackalen@lcs.on.ca>
2025-01-20 08:59:01 -0500

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Hannu Packalen on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
