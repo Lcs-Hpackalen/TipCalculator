2368eb5fe0e25b8bfc04d1c463265100a5db1632
Hannu Packalen <hpackalen@lcs.on.ca>
2025-01-20 09:08:37 -0500

    Initial project orginization complete.

---
+++ TipCalculator/Views/TippingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Hannu Packalen on 2025-01-20.
+//
+
+import SwiftUI
+
+struct TippingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TippingView()
+}
